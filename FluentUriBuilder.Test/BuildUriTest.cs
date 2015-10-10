a7d123ba5a803ca41595cccd68a19051da12c7ae
Botond Balazs <balazsbotond@gmail.com>
2015-10-10 11:13:24 +0200

    Create empty project skeleton

---
+++ FluentUriBuilder.Test/BuildUriTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+using FluentUriBuilder;
+
+namespace FluentUriBuilder.Test
+{
+    public class BuildUriTest
+    {
+        [Fact]
+        public void FromReturnsFluentUriBuilderInstance()
+        {
+            BuildUri.From(string.Empty).Should().NotBeNull();
+        }
+   } 
+}
---
+++ FluentUriBuilder/BuildUri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentUriBuilder
+{
+    public static class BuildUri
+    {
+        public static FluentUriBuilder From(string baseUri)
+        {
+            return new FluentUriBuilder(baseUri);
+        }
+    }
+}
