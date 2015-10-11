627b2447dcc2871979193e5339df7d5c395bdfcf
Botond Balazs <balazsbotond@gmail.com>
2015-10-11 20:13:12 +0200

    Add a `publish-nuget.cmd` to help publish new versions of the package

---
+++ publish-nuget.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+echo(
+echo Creating version '%1'.
+echo(
+echo Please open FluentUriBuilder.nuspec and update it manually.
+echo(
+pause
+
+echo Packing FluentUriBuilder v%1...
+nuget pack FluentUriBuilder\FluentUriBuilder.csproj -Prop Configuration=Release
+
+echo(
+echo Press any key to publish FluentUriBuilder.%1.nupkg.
+echo(
+pause
+nuget push FluentUriBuilder.%1.nupkg
